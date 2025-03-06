350c7e3f256f0c1ab5d70ff8ce0e35f983f077b2
jqssun <jqssun+git@googlemail.com>
2025-03-06 01:12:34 +0000

    app: com.atharok.btremote

---
+++ app/com.atharok.btremote.sh
@@ -0,0 +1,15 @@
+ID="com.atharok.btremote"
+REPO="https://gitlab.com/Atharok/BtRemote.git"
+COMMIT="main" # a7d894078cff537150daba51c78a44c5bb671983
+SUBDIR="app"
+FLAVOR="default"
+
+# this build enables support for using the device as a BT HID device for a PC
+build_init() {
+    gsed -i 's@sendReport(KEYBOARD_REPORT_ID, bytes)@sendReport(KEYBOARD_REPORT_ID, byteArrayOf(0x00) + bytes + byteArrayOf(0x00, 0x00, 0x00, 0x00))@' app/src/main/java/com/atharok/btremote/presentation/viewmodel/BluetoothHidViewModel.kt
+    gsed -i 's@sendReport(MOUSE_REPORT_ID, bytes)@sendReport(MOUSE_REPORT_ID, bytes + byteArrayOf(0x00))@' app/src/main/java/com/atharok/btremote/presentation/viewmodel/BluetoothHidViewModel.kt
+    gsed -i 's@MOUSE_REPORT_ID = 0x03@MOUSE_REPORT_ID = 0x02@' app/src/main/java/com/atharok/btremote/common/utils/Constants.kt
+}
+
+# for debugging: to sniff BT traffic on macOS, use PacketLogger from Xcode Additional Tools, then install the profile 
+# https://developer.apple.com/bug-reporting/profiles-and-logs/?name=bluetooth
